3912334a4924e3c913be06a9a18fd3cf2800e7b9
JulianSandoval25 <juliansandoval1996@gmail.com>
2023-02-05 21:25:06 -0300

    add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
 import express, { json } from "express";
+
+import cors from "cors";
+
+
 const app = express();
 
 import router from './router/ahorcado.js'
 
-
+app.use(cors());
 app.use(express.json())
 
 const port=process.env.PORT || 3000;
