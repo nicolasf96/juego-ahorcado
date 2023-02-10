55fc386aaeeba0d048a2793b126a089cf77679a2
JulianSandoval25 <juliansandoval1996@gmail.com>
2023-02-10 10:47:25 -0300

    add dev

--- src/index.js
+++ src/index.js
@@ -6,16 +6,20 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+
+const isProduction= process.env.NODE_ENV === "production";
+const distPatch = isProduction ? "../dist/prod" : "../dist/dev";
+
+
 
 const port=process.env.PORT || 3000;
 
 app.use("/", router);
-app.use(express.static(path.join(__dirname, '../dist/juego-ahorcado')));
+app.use(express.static(path.join(__dirname, distPatch)));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/juego-ahorcado/index.html'));
+  res.sendFile(path.join(__dirname, distPatch+'/index.html'));
 });
 
 app.listen(port,()=>{
